14b6d14e111512e793be08e35d5ab4598aca6bb5
zhuyu <zhuyu@haoduo.vip>
2021-07-07 23:29:55 +0800

    add

---
+++ public/precache-manifest.5765af38e3ef49e008a286b32a7859fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00bf90860cafa44f6f5",
+    "url": "/static/css/main.3d7f18bc.chunk.css"
+  },
+  {
+    "revision": "e00bf90860cafa44f6f5",
+    "url": "/static/js/main.e00bf908.chunk.js"
+  },
+  {
+    "revision": "94b03981056c12f13653",
+    "url": "/static/js/1.94b03981.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f751c040a2fdbdda3010475ed128f86b",
+    "url": "/static/media/spacex-logo-light.f751c040.png"
+  },
+  {
+    "revision": "cf0d6e3e91b96e12faaf6360d7b669b8",
+    "url": "/index.html"
+  }
+];
